f43f03a7eb5908a52fa77c70bf3b672cb8a8e711
Daniels <daniel.sadowski75@gmail.com>
2019-02-06 21:34:26 +0100

    Step 1.3 directive - 2 different messages

--- src/app/myexamples/component-with-popup.component.ts
+++ src/app/myexamples/component-with-popup.component.ts
@@ -3,11 +3,11 @@ import {Component} from "@angular/core";
 @Component({
   selector: 'component-with-popup',
   template: `
-      <div class="ui message" popup message='Clicked message from host element'>
+      <div class="ui message" popup message='Clicked message host element'>
         <div class="header">Learning Directives</div>
         <p>This should use a Popup directive</p>
       </div>
-    <i class="alarm icon" popup></i>
+    <i class="alarm icon" popup message='Clicked popup host element'></i>
   `
 })
 export class ComponentWithPopupComponent {
