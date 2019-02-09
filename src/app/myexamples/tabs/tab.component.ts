979a9a4935a4681156372c8671ad6c1d23aa40da
Daniels <daniel.sadowski75@gmail.com>
2019-02-09 17:37:11 +0100

    Step 3.2 added name and active properties to Tab

--- src/app/myexamples/tabs/tab.component.ts
+++ src/app/myexamples/tabs/tab.component.ts
@@ -7,6 +7,8 @@ import {Component, Input, OnInit} from "@angular/core";
 })
 export class TabComponent implements OnInit {
   @Input() title: string;
+  @Input() name: string;
+  active: boolean = false;
 
   ngOnInit() {
 
